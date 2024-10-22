16c353f736b018480bdd9edb514b79463243485a
hoangdan123pg <dannhhe173549@fpt.edu.vn>
2024-10-22 10:45:31 +0700

    Update Home.jsx

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,14 @@
 export default function Home(){
     return (
+<<<<<<< Updated upstream
         <h1 className="text-orange">Hello Dan</h1>
+=======
+
+        <div>
+        <h1 className="text-orange">Hello By</h1>
+        
+        </div>
+
+>>>>>>> Stashed changes
     )
 }
