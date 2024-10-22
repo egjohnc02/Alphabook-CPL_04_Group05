2aa28a3febe7b80c38ebdbf3015dcecfc72aa30c
egjohnc02 <egjohnc02@gmail.com>
2024-10-22 10:10:59 +0700

    Update Home.jsx

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,10 @@
 export default function Home(){
     return (
+        <div>
         <h1 className="text-orange">Hello</h1>
+        <h1 className="text-orange">Hello</h1>
+        <h1 className="text-orange">Hello</h1>
+
+        </div>
     )
 }
