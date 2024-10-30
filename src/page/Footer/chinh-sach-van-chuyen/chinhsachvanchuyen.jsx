e7916a4b858dff8b5a429ab09442a138fa1eeab3
HoangTran <hoangluffy098@gmail.com>
2024-10-31 00:19:58 +0700

    Commit Footer

---
+++ src/page/Footer/chinh-sach-van-chuyen/chinhsachvanchuyen.jsx
@@ -0,0 +1,48 @@
+// import bgLogin from "../../../assets/bg_login.webp";
+function ChinhSachVanChuyen() {
+  return (
+    <div>
+      <div>
+        {/* <div className="position-relative text-center">
+          <img
+            src={bgLogin}
+            alt="Example"
+            className="img-fluid"
+            style={{ maxWidth: "100%" }}
+          />
+          <div className="position-absolute top-50 start-50 translate-middle">
+            <a href="/home">
+              <span>Trang chủ</span>
+            </a>
+            <h1 style={{ color: "orange" }}>Chính sách vận chuyển</h1>
+          </div>
+        </div> */}
+        <section className="page">
+          <div className="container">
+            <div className="wrap_background_aside padding-top-15 margin-bottom-40">
+              <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-12">
+                  <h1 className="title-block-page">Chính sách vận chuyển</h1>
+                  <div className="content-page rte">
+                    <p>
+                      <span
+                        style={{ fontFamily: "Tahoma, Geneva, sans-serif" }}
+                      >
+                        <span style={{ fontSize: "16px" }}>
+                          Phí vận chuyển có thể thay đổi với từng khu vực, nhân
+                          viên xử lý đơn hàng sẽ gọi điện trao đổi và thống nhất
+                          với khách hàng trước khi giao.
+                        </span>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+export default ChinhSachVanChuyen;
---
+++ src/page/Footer/he-thong-nha-sach/hethongnhasach.jsx
@@ -0,0 +1,88 @@
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import bgLogin from "../../../assets/bg_login.webp";
+import axios from "axios";
+import "./hethongnhasach.css";
+
+function hethongnhasach() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9999/DiemBanHang")
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  return (
+    <div>
+      <div>
+        {/* <div className="position-relative text-center">
+          <img
+            src={bgLogin}
+            alt="Example"
+            className="img-fluid"
+            style={{ maxwidth: "100%" }}
+          />
+          <div className="position-absolute top-50 start-50 translate-middle">
+            <a href="/home">
+              <span>Trang chủ</span>
+            </a>
+            <h1 style={{ color: "orange" }}>Hệ thống nhà sách</h1>
+          </div>
+        </div> */}
+        <div className="content-page rte">
+          <table
+            className="display dataTable no-footer"
+            border="1"
+            cellPadding="1"
+            cellSpacing="1"
+          >
+            <thead>
+              <tr role="row">
+                <th className="sorting_asc">Điểm bán hàng</th>
+                <th className="sorting_asc">
+                  <strong>Nhà sách</strong>
+                </th>
+                <th className="sorting">
+                  <strong>Địa chỉ</strong>
+                </th>
+                <th className="sorting">
+                  <strong>Thành Phố</strong>
+                </th>
+                <th className="sorting">
+                  <strong>SĐT</strong>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+
+                
+                    {data.map((item, index) => (
+                        <tr key={index} className="even" role="row">
+                            <td className="sorting_1">{item.DiemBanHang}</td>
+                            <td className="sorting_1">{item.NhaSach}</td>
+                            <td>{item.DiaChi}</td>
+                            <td className="sorting_1">{item.ThanhPho}</td>
+                            <td>{item.SDT}</td>
+                        </tr>
+                    ))}
+                </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default hethongnhasach;
