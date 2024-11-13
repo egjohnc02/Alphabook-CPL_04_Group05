a6dd54db5c316c31c46ba50002e47acdb20fc481
Vuducduy0503 <vuducduy0503@gmail.com>
2024-11-13 13:24:35 +0700

    update

--- src/page/Account/Customer/DonHang/DonHang.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DonHang: React.FC = () => {
-  return <p className="fs-4">Đơn hàng của bạn</p>;
-};
-
-export default DonHang;
--- src/utils/AutoScrollToTop.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export default function AutoScrollToTop(): null {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  return null;
-}
