78e91ca25b27c7a8368e09c66e0d763ebb1d7e09
egjohnc02 <egjohnc02@gmail.com>
2024-11-19 21:09:52 +0700

    update search book

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -127,7 +127,7 @@ const Navbar: React.FC = () => {
         )}
 
         <div className='d-flex'>
-          <Link to='/book' className='text-decoration-none text-dark fw-medium'>
+          <Link to='/search' className='text-decoration-none text-dark fw-medium'>
             <div className='border-end border-black border-1 pe-3'>
               <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
